e4afc2ff9c0d758371759e3fe1e9a88f0417ae7e
zequihg50 <zequi3000@hotmail.com>
2019-10-29 10:29:57 +0100

    Added evaluation to cordex query

--- ESGF-inventory/cordex/query.sh
+++ ESGF-inventory/cordex/query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE_URL='https://esgf-data.dkrz.de/esg-search/search?format=application%2Fsolr%2Bjson&latest=true&replica=false&type=Dataset'
-QUERIES="project=CORDEX&experiment=historical,rcp26,rcp45,rcp85&domain=WAS-44,SAM-44|experiments.csv"
+QUERIES="project=CORDEX&experiment=historical,evaluation,rcp26,rcp45,rcp85&domain=WAS-44,SAM-44|experiments.csv"
 
 FIELDS="fields=master_id,experiment,size"
 FACETS="facets=activity_id,source_id,institution_id,source_type,nominal_resolution,experiment_id,variant_label,grid_label,table_id,frequency,realm,variable_id"
@@ -30,5 +30,5 @@ query_esgf() {
 for q in $QUERIES
 do
 	dest="${q##*|}"
-	query_esgf "${q%|*}" | jq --slurp 'map(. + { group: (.master_id|split(".")|del(.[5,10])|join(".")),master_id})    | group_by(.group)       | map(   reduce .[] as $item ({experiments: []}; {group: $item.group, experiments: (.experiments + $item.experiment)}  )   )'  |jq -r 'map({group, historical: .experiments|join(" ")|test("\\bhistorical\\b"), rcp26: .experiments|join(" ")|test("\\brcp26\\b"), rcp45: .experiments|join(" ")|test("\\brcp45\\b"), rcp85: .experiments|join(" ")|test("\\brcp85\\b")})' | jq -r '(map(keys) | add | unique) as $cols   |     map(. as $row | $cols | map($row[.])) as $rows  |   $cols, $rows[]  |@csv' > ${dest}
+	query_esgf "${q%|*}" | jq --slurp 'map(. + { group: (.master_id|split(".")|del(.[5,10])|join(".")),master_id})    | group_by(.group)       | map(   reduce .[] as $item ({experiments: []}; {group: $item.group, experiments: (.experiments + $item.experiment)}  )   )'  |jq -r 'map({group, historical: .experiments|join(" ")|test("\\bhistorical\\b"), rcp26: .experiments|join(" ")|test("\\brcp26\\b"), rcp45: .experiments|join(" ")|test("\\brcp45\\b"), rcp85: .experiments|join(" ")|test("\\brcp85\\b"), evaluation: .experiments|join(" ")|test("\\bevaluation\\b")})' | jq -r '(map(keys) | add | unique) as $cols   |     map(. as $row | $cols | map($row[.])) as $rows  |   $cols, $rows[]  |@csv' | awk -F "," 'BEGIN{OFS=","}{print $2,$3,$1,$4,$5,$6}' >	${dest}
 done
