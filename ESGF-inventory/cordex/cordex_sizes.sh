b5ffb03065540ddfa29463a81d54021705ceb58e
zequihg50 <ezequiel.cimadevilla@unican.es>
2020-01-24 16:20:23 +0100

    Fixed errata in cordex_sizes.sh

--- ESGF-inventory/cordex/cordex_sizes.sh
+++ ESGF-inventory/cordex/cordex_sizes.sh
@@ -12,4 +12,4 @@ echo -n 'Variables tas: '
 query_esgf "project=CORDEX&product=output&experiment=${experiments}&time_frequency=${frequencies}&variable=tas" 2>/dev/null | jq --slurp 'map(.size)|add' | numfmt --to=iec
 
 echo -n 'EUR domains (11,22,44): '
-query_esgf "project=CORDEX&product=output&experiment=${experiments}&time_frequency=${frequencies}&domain=EUR11,EUR-22,EUR-44" 2>/dev/null | jq --slurp 'map(.size)|add' | numfmt --to=iec
+query_esgf "project=CORDEX&product=output&experiment=${experiments}&time_frequency=${frequencies}&domain=EUR-11,EUR-22,EUR-44" 2>/dev/null | jq --slurp 'map(.size)|add' | numfmt --to=iec
