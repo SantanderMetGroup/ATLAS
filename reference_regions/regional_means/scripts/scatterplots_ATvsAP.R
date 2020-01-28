6ea8b1898f53fe44e9f70d25f6433e6723db9530
Your Name <maibide@gmail.com>
2020-01-28 16:28:19 +0100

    update

--- reference_regions/regional_means/scripts/scatterplots_ATvsAP.R
+++ reference_regions/regional_means/scripts/scatterplots_ATvsAP.R
@@ -1,7 +1,6 @@
 library(magrittr)
 library(httr)
 
-
 # list files in Url -----------------------------------------------------------------------------
 
 # https://stackoverflow.com/questions/25485216/how-to-get-list-files-from-a-github-repository-folder-using-r
@@ -13,16 +12,17 @@ root <- "https://raw.githubusercontent.com/SantanderMetGroup/ATLAS/devel/"
 
 # select reference and target periods, scenarios, and the area ("land", "sea", "landsea"):-----------
 ref.period <- 1995:2014
+season <- c(12, 1, 2)
 periods <- list(2021:2040, 2041:2060, 2061:2080, 2081:2100)
 exp <- c("rcp26", "rcp45", "rcp85") #c("ssp126", "ssp245", "ssp585")
 area <- "land" #sea #landsea
-project <- "CMIP5" #CMIP6Amon
+project <- "CMIP5" #"CMIP6Amon"
 
 
 
 ##### function to get the data and compute deltas: -------------------------------
 
-computeDeltas <- function(allfiles, modelruns, exp, ref.period, periods){ 
+computeDeltas <- function(allfiles, modelruns, exp, ref.period, periods, season){ 
   var <- scan(allfiles[1], "character", n = 7)[4]
   region <- colnames(read.table(allfiles[1], header = TRUE, sep = ",", skip = 7))[-1]
   aggrfun <- "mean"
@@ -30,9 +30,21 @@ computeDeltas <- function(allfiles, modelruns, exp, ref.period, periods){
   out <- lapply(modelruns, function(i) {
     modelfiles <- grep(i, allfiles, value = TRUE) 
     hist <- grep("historical", modelfiles, value = TRUE) %>% read.table(header = TRUE, sep = ",", skip = 7)
-    yrs <- grep("historical", modelfiles, value = TRUE) %>% read.table(header = TRUE, sep = ",", skip = 7) %>% subset(select = "date", drop = TRUE) %>% gsub("-.*", "", .) %>% as.integer()
+    seas <- hist %>% subset(select = "date", drop = TRUE) %>% gsub(".*-", "", .) %>% as.integer()
+    z <- sort(unlist(lapply(season, function(s) which(seas == s))))
+    hist <- hist[z, ]
+    yearshist <-  unique(hist %>% subset(select = "date", drop = TRUE) %>% gsub("-.*", "", .) %>% as.integer())
+    firstind <- which(seas[z] == season[1])[1]
+    if (firstind > 1) {
+      yrs <- c(rep(1, firstind-1), rep(2:ceiling(nrow(hist)/length(season)+1), each = length(season), length.out = nrow(hist)-(firstind-1)))
+      yearshist <- c(yearshist, yearshist[length(yearshist)] + 1)
+    } else {
+      yrs <-  hist %>% subset(select = "date", drop = TRUE) %>% gsub("-.*", "", .) %>% as.integer()
+      yearshist <-  unique(yrs)
+    }
     hist <- lapply(split(hist[,-1], f = yrs), function(x) apply(x, MARGIN = 2, FUN = aggrfun, na.rm = TRUE))
     hist <- do.call("rbind", hist)
+    rownames(hist) <- yearshist[1:nrow(hist)]
     start <- which(rownames(hist) == range(ref.period)[1])
     end <- which(rownames(hist) == range(ref.period)[2])
     fill <- FALSE
@@ -46,11 +58,23 @@ computeDeltas <- function(allfiles, modelruns, exp, ref.period, periods){
         grep(exp[j], modelfiles, value = TRUE) %>% read.table(header = TRUE, sep = ",", skip = 7)
       }, error = function(err) return(NULL))
       dates <- tryCatch({
-        yrs <- grep(exp[j], modelfiles, value = TRUE) %>% read.table(header = TRUE, sep = ",", skip = 7) %>% subset(select = "date", drop = TRUE) %>% gsub("-.*", "", .) %>% as.integer()
+        seas <- rcp %>% subset(select = "date", drop = TRUE) %>% gsub(".*-", "", .) %>% as.integer()
+        z <- sort(unlist(lapply(season, function(s) which(seas == s))))
+        rcp <- rcp[z, ]
+        yearsrcp <-  unique(rcp %>% subset(select = "date", drop = TRUE) %>% gsub("-.*", "", .) %>% as.integer())
+        firstind <- which(seas[z] == season[1])[1]
+        if (firstind > 1) {
+          yrs <- c(rep(1, firstind-1), rep(2:ceiling(nrow(rcp)/length(season)+1), each = length(season), length.out = nrow(rcp)-(firstind-1)))
+          yearsrcp <- c(yearsrcp, yearsrcp[length(yearsrcp)] + 1)
+        } else {
+          yrs <-  rcp %>% subset(select = "date", drop = TRUE) %>% gsub("-.*", "", .) %>% as.integer()
+          yearsrcp <-  unique(yrs)
+        }
         rcp <- lapply(split(rcp[,-1], f = yrs), function(x) apply(x, MARGIN = 2, FUN = aggrfun, na.rm = TRUE))
       }, error = function(err) return(NULL))
       if (!is.null(rcp)) {
         rcp <- do.call("rbind", rcp)
+        rownames(rcp) <- yearsrcp[1:nrow(rcp)]
         if (fill) {
           message("i =", i, ".......", exp[j], "------filling reference period with rcp data")
           rcphist <- rcp[which(rownames(rcp) == 2006) : which(rownames(rcp) == range(ref.period)[2]),]
@@ -61,14 +85,14 @@ computeDeltas <- function(allfiles, modelruns, exp, ref.period, periods){
         }
         delta <- lapply(periods, function(k){
           endyear <- range(k)[2]
-          while (length(which(rownames(rcp) == endyear)) == 0) {
+          while (length(which(yearsrcp == endyear)) == 0) {
             endyear <- endyear - 1
           }
-          rcpk <- rcp[which(rownames(rcp) == range(k)[1]) : which(rownames(rcp) == endyear),]
+          rcpk <- rcp[which(yearsrcp == range(k)[1]) : which(yearsrcp == endyear),]
           if (var == "tas") {
             apply(rcpk, MARGIN = 2, FUN = mean, na.rm = TRUE) - histexp
           } else if (var == "pr") {
-            (apply(rcpk, MARGIN = 2, FUN = mean, na.rm = TRUE) - histexp)/ histexp * 100
+            (apply(rcpk, MARGIN = 2, FUN = mean, na.rm = TRUE) - histexp) / histexp * 100
           }
         })
       } else {
@@ -109,7 +133,7 @@ aux <- grep("historical", ls, value = TRUE)
 modelruns <- gsub(paste0("reference_regions/regional_means/data/", project, "_tas_", area, "/",project, "_|_historical.csv"), "", aux)
 
 # compute (apply computeDeltas)
-tas <- computeDeltas(allfiles, modelruns, exp, ref.period, periods)
+tas <- computeDeltas(allfiles, modelruns, exp, ref.period, periods, season)
 #str(tas)
 
 # calculate percentiles
@@ -120,12 +144,12 @@ tasp10 <- lapply(tas, apply, 2, quantile, 0.1, na.rm = T)
 
 
 ######## pr ###########------------------------------------
-ls <- grep(paste0("CMIP6Amon_pr_", area, "/"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
+ls <- grep(paste0(project, "_pr_", area, "/"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
 allfiles <- paste0(root, ls)
 aux <- grep("historical", ls, value = TRUE)
 modelruns <- gsub(paste0("reference_regions/regional_means/data/", project, "_pr_", area, "/",project, "_|_historical.csv"), "", aux)
 
-pr <- computeDeltas(allfiles, modelruns, exp, ref.period, periods)
+pr <- computeDeltas(allfiles, modelruns, exp, ref.period, periods, season)
 
 prmediana <- lapply(pr, apply, 2, median, na.rm = T)
 prp90 <- lapply(pr, apply, 2, quantile, 0.9, na.rm = T)
@@ -137,7 +161,7 @@ if (area == "land") region.subset <- names(tas)[c(1:44, 56)]
 if(area == "sea") region.subset <-  names(tas)[44:56]
 
 #select the output figure file name
-outfilename <- paste0(project, "_scatterplots_", area, "_ATvsAP.pdf")
+outfilename <- paste0(project, "_scatterplots_", area, "_", paste(season, collapse = "-"), "_ATvsAP.pdf")
 
 #plot and write figure
 pdf(outfilename, width = 20, height = 25)
@@ -156,4 +180,9 @@ for(i in region.subset) {
   points(tasmediana[[i]], prmediana[[i]], pch = 21, bg = c(rep(rgb(0,0,0.6),4), rep(rgb(0.2,0.6,1),4), rep(rgb(1,0,0),4)), xlim = c(0, 7))
 }
 dev.off()
+
+save(list=c("tas", "pr"), file = paste0(project, "_scatterplots_", area, "_", paste(season, collapse = "-"), "_ATvsAP.rda"))
+
+
+
 
