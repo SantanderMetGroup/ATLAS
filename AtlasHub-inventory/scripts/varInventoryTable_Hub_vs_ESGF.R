ceb8471a33d7baa30617bee8052c9e234c10ff1c
Your Name <maibide@gmail.com>
2020-04-22 10:49:41 +0200

    update inventories

--- AtlasHub-inventory/scripts/varInventoryTable_Hub_vs_ESGF.R
+++ AtlasHub-inventory/scripts/varInventoryTable_Hub_vs_ESGF.R
@@ -5,8 +5,9 @@ library(lattice)
 # USER PARAMETER SETTING -----------------------------------------------------------------------
 
 # datasets
-datasets <- "CMIP6"# "CMIP6Amon"
-
+datasets <- "CMIP6Amon"# "CMIP6"
+a <- "day"
+if (datasets == "CMIP6Amon") a <- "mon"
 # output directory, e.g.:
 out.dir <- "AtlasHub-inventory/Hub/Hub_vs_ESGF"
 
@@ -21,10 +22,11 @@ root <- "https://raw.githubusercontent.com/SantanderMetGroup/ATLAS/devel/"
 # list the subset of target files
 ls <- grep(paste0("ESGF-inventory/", gsub("Amon", "", datasets), "/", datasets, ".csv"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
 esgffile <- paste0(root, ls)
-ls <- grep(paste0("AtlasHub-inventory/Hub/", datasets, "_"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
+ls <- grep(paste0("AtlasHub-inventory/Hub/", gsub("Amon", "", datasets), "_", a), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
 hubfile <- paste0(root, ls)
 
 # FIND MATCHES AND EXPORT PDF AND CSV -------------------------------------------------------------
+
 hub <- read.csv(hubfile)
 esgf <- read.csv(esgffile)
 x.hub <- strsplit(as.character(hub[,1]), "_")
@@ -32,17 +34,24 @@ x.hub <- lapply(x.hub, "[", c(2,4,3))
 x.hub <- lapply(x.hub, function(i) paste0(paste(i, collapse = ".*"), ".*"))
 x.esgf <- as.character(esgf[,1])
 
-esgf[,-1] <- data.frame(lapply(esgf[,-1], as.logical))
+esgf[,-(1:2)] <- data.frame(lapply(esgf[,-(1:2)], as.logical))
 diff <- esgf
-nam <- colnames(esgf[,-1])
+nam <- colnames(esgf[,-(1:2)])
 for (i in 1:nrow(hub)) {
   k <- grep(x.hub[i], esgf[,1])
   if (length(k) < 1) k <- NA
+  if (length(k) > 1) {
+    choices <- sapply(strsplit(as.character(esgf[k, 1]), "\\."), "[", 4)
+    ch <- sapply(choices, grep, x.hub[i])  
+    k <- k[which(ch == 1)]
+  }
   if (!is.na(k)) {
     for (j in nam) {
       exists <- esgf[k, j]
       if (exists) {
-        if (hub[i, j]) {
+        if (is.null(hub[i, j])){
+          diff[k, j] <- 0L
+        } else if (hub[i, j]) {
           diff[k, j] <- 1L
         } else {
           diff[k, j] <- 0L
@@ -55,12 +64,13 @@ for (i in 1:nrow(hub)) {
 }
 
 # csv
-write.csv(diff, paste0(out.dir, "/", gsub(paste0(".*", datasets), datasets, ls)))
+filename <- paste0(out.dir, "/", gsub(".csv", paste0("_", gsub("-", "", Sys.Date()), ".csv"), gsub(".*/", "", ls)))
+write.csv(diff, filename)
 
 # figure
-diff.p <- as.matrix(diff[,-1])
+diff.p <- as.matrix(diff[,-(1:2)])
 rownames(diff.p) <- as.character(diff[,1])
-pdf(paste0(out.dir, "/", gsub(".csv", ".pdf", gsub(paste0(".*", datasets), datasets, ls))), width = 5, height = 60)
+pdf(gsub(".csv", ".pdf", filename), width = 5, height = 120)
 levelplot(t(diff.p), scales=list(x=list(alternating=2, rot=90, cex = 0.5),
                                      y=list(cex = 0.5)),
                   border = "black", bw = 10, ylab = NULL, colorkey = FALSE,
