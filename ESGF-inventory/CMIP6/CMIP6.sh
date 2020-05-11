8ade5730a3195f40db6dd76031817a199c030f44
zequihg50 <ezequiel.cimadevilla@unican.es>
2020-05-11 12:47:15 +0200

    Refactored CMIP6.sh and added daily_one_run.csv

--- ESGF-inventory/CMIP6/CMIP6.sh
+++ ESGF-inventory/CMIP6/CMIP6.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-query="project=CMIP6 activity_id=CMIP,ScenarioMIP experiment_id=historical,esm-hist,ssp126,ssp245,ssp370,ssp585,ssp460 variable_id=sftlf,pr,tas,tasmin,tasmax,psl"
+variables="sftlf,pr,tas,tasmin,tasmax,psl"
+query="project=CMIP6 activity_id=CMIP,ScenarioMIP experiment_id=historical,esm-hist,ssp126,ssp245,ssp370,ssp585,ssp460 variable_id=${variables}"
 fields="master_id,variable,size"
 
 to_inventory() {
-	jq --slurp '
+	jq -r --slurp --arg variables "${variables}" '
 		map(. + { dataset_id: (.master_id|split(".")|del(.[6,7])|join(".")),master_id}) |
 		group_by(.dataset_id) |
 		map(reduce .[] as $item (
-			{variables: [], size: 0};
-			{dataset_id: $item.dataset_id, variables: (.variables + $item.variable), size: (.size + $item.size)}))' | jq -r '
-		map({	dataset_id,
-				size,
-				tasmax: .variables|join(" ")|test("\\btasmax\\b"),
-				tasmin: .variables|join(" ")|test("\\btasmin\\b"),
-				tas: .variables|join(" ")|test("\\btas\\b"),
-				pr: .variables|join(" ")|test("\\bpr\\b"),
-				psl: .variables|join(" ")|test("\\bpsl\\b"),
-				sftlf: .variables|join(" ")|test("\\bsftlf\\b")})' | jq -r '
-		["dataset_id", "size", "pr", "psl", "tas", "tasmax", "tasmin", "sftlf"] as $cols |
-		map(. as $row | $cols | map($row[.])) as $rows |
-		$cols, $rows[] |@csv' 
+		    ($variables|split(",")|map({(.): false})|reduce .[] as $i ({size: 0}; . + $i));
+		    . + { dataset_id: $item.dataset_id, size: (.size + $item.size), ($item.variable|first): true } )) |
+		(["dataset_id", "size"] + ($variables|split(",")|sort)) as $keys | $keys, map([.[ $keys[] ]])[] | @csv'
 }
 
-../esgf-search -f $fields $query table_id=Amon,fx frequency=fx,mon | to_inventory > CMIP6Amon.csv
-../esgf-search -f $fields $query table_id=day,fx frequency=fx,day | to_inventory > CMIP6day.csv
+../esgf-search -i "esgf-node.llnl.gov" -f $fields $query table_id=Amon,fx frequency=fx,mon | to_inventory > CMIP6Amon.csv
+../esgf-search -i "esgf-node.llnl.gov" -f $fields $query table_id=day,fx frequency=fx,day | to_inventory > CMIP6day.csv
