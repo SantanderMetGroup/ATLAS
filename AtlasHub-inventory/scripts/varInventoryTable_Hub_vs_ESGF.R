66707e6deeedac82a0b32187bc2901e1a63d47ee
Your Name <maibide@gmail.com>
2020-05-14 09:38:14 +0200

    update

--- AtlasHub-inventory/scripts/varInventoryTable_Hub_vs_ESGF.R
+++ AtlasHub-inventory/scripts/varInventoryTable_Hub_vs_ESGF.R
@@ -5,12 +5,10 @@ library(lattice)
 # USER PARAMETER SETTING -----------------------------------------------------------------------
 
 # datasets
-datasets <- "CMIP6Amon"# "CMIP6"
-a <- "day"
-if (datasets == "CMIP6Amon") a <- "mon"
+datasets <- "CMIP6_mon"# "CMIP6_day"
 # output directory, e.g.:
 out.dir <- "AtlasHub-inventory/Hub/Hub_vs_ESGF"
-
+extension <- "" #"_1run" 
 
 # LIST FILES IN URL -----------------------------------------------------------------------------
 # https://stackoverflow.com/questions/25485216/how-to-get-list-files-from-a-github-repository-folder-using-r
@@ -20,9 +18,9 @@ filelist <- unlist(lapply(content(req)$tree, "[", "path"), use.names = FALSE)
 root <- "https://raw.githubusercontent.com/SantanderMetGroup/ATLAS/devel/"
 
 # list the subset of target files
-ls <- grep(paste0("ESGF-inventory/", gsub("Amon", "", datasets), "/", datasets, ".csv"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
+ls <- grep(paste0("ESGF-inventory/CMIP6/", datasets, extension,".csv"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
 esgffile <- paste0(root, ls)
-ls <- grep(paste0("AtlasHub-inventory/Hub/", gsub("Amon", "", datasets), "_", a), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
+ls <- grep(paste0("AtlasHub-inventory/Hub/", datasets, "_Hub.csv"), filelist, value = TRUE, fixed = TRUE) %>% grep("\\.csv$", ., value = TRUE)
 hubfile <- paste0(root, ls)
 
 # FIND MATCHES AND EXPORT PDF AND CSV -------------------------------------------------------------
@@ -40,6 +38,7 @@ nam <- colnames(esgf[,-(1:2)])
 for (i in 1:nrow(hub)) {
   k <- grep(x.hub[i], esgf[,1])
   if (length(k) < 1) k <- NA
+  
   if (length(k) > 1) {
     choices <- sapply(strsplit(as.character(esgf[k, 1]), "\\."), "[", 4)
     ch <- sapply(choices, grep, x.hub[i])  
@@ -64,18 +63,18 @@ for (i in 1:nrow(hub)) {
 }
 
 # csv
-filename <- paste0(out.dir, "/", gsub(".csv", paste0("_", gsub("-", "", Sys.Date()), ".csv"), gsub(".*/", "", ls)))
+filename <- paste0(out.dir, "/", gsub(".csv", paste0("_vs_ESGF", extension, "_", gsub("-", "", Sys.Date()), ".csv"), gsub(".*/", "", ls)))
 write.csv(diff, filename)
 
 # figure
-diff.p <- as.matrix(diff[,-(1:2)])
-rownames(diff.p) <- as.character(diff[,1])
+diff.p <- as.matrix(diff[nrow(diff):1,-(1:2)])
+rownames(diff.p) <- as.character(diff[nrow(diff):1,1])
 pdf(gsub(".csv", ".pdf", filename), width = 5, height = 120)
 levelplot(t(diff.p), scales=list(x=list(alternating=2, rot=90, cex = 0.5),
-                                     y=list(cex = 0.5)),
-                  border = "black", bw = 10, ylab = NULL, colorkey = FALSE,
-                  xlab = "",
-                  col.regions = c("red", "green"),
-                  main = list("green = available    red = not available   white = not available in ESGF", cex = 0.8))
+                                 y=list(cex = 0.5)),
+          border = "black", bw = 10, ylab = NULL, colorkey = FALSE,
+          xlab = "",
+          col.regions = c("red", "green"),
+          main = list("green = available    red = not available   white = not available in ESGF", cex = 0.8))
 dev.off()
 
