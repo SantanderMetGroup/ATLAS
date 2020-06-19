4bcc60e147c65d51ec30e8a22830f05a9d878d69
zequihg50 <ezequiel.cimadevilla@unican.es>
2020-06-19 15:20:04 +0200

    Split CORDEX metalinks because of various issues

--- ESGF-inventory/CORDEX/CORDEX.sh
+++ ESGF-inventory/CORDEX/CORDEX.sh
@@ -15,6 +15,7 @@ to_inventory() {
 		(["dataset_id", "size"] + ($variables|sort)) as $keys | $keys, map([.[ $keys[] ]])[] | @csv'
 }
 
+# Generate csv inventories, one invetory per time_frequency
 jq 'select(.replica|not)|select(.time_frequency|first != "fx")' CORDEX.json | to_inventory > CORDEX_day.csv
 jq 'select(.replica|not)|select(.time_frequency|first == "fx")' CORDEX.json | to_inventory > CORDEX_fx.csv
 
@@ -22,6 +23,19 @@ jq 'select(.replica|not)|select(.time_frequency|first == "fx")' CORDEX.json | to
 domains='["SAM-20","SAM-44","SAM-22","CAM-44","CAM-22","NAM-11","NAM-22","NAM-44","AFR-22","AFR-44","WAS-22","WAS-44","EAS-22","EAS-44","CAS-22","CAS-44","AUS","AUS-22","AUS-44","ANT-44","ARC-44","SEA-22","MNA-22","MNA-44"]'
 variables='["pr","tas","tasmax","tasmin","huss","scfWind","sftlf","orog"]'
 
+# ../esgf-metalink < CORDEX.json > metalink # This would generate the metalink for all files in inventory
 jq --argjson variables $variables --argjson domains $domains '
   select(.variable|first|IN($variables[])) |
   select(.domain|first|IN($domains[]))' CORDEX.json | ../esgf-metalink > metalink
+
+# We create a directory to hold all metalinks
+mkdir -p metalinks
+
+# Data from esg-dn1.ru.ac.th is commercial
+sed -n '1{p}; ${p}; /<file name/{h;n}; H; /<\/file>/{x; /esg-dn1.ru.ac.th/{p}}' metalink > metalinks/esg-dn1.ru.ac.th.metalink
+
+# Data from esgf-ictp.hpc.cineca.it has incorrect size on the server so we remove <size> element from metalink
+sed -n '1{p}; ${p}; /<file name/{h;n}; H; /<\/file>/{x; /esgf-ictp.hpc.cineca.it/{s|<size>.*</size>||;p}}' metalink > metalinks/esgf-ictp.hpc.cineca.it.metalink
+
+# Remove all previous cases from general metalink
+sed -n '1{p}; ${p}; /<file name/{h;n}; /\(esg-dn1.ru.ac.th\|esgf-ictp.hpc.cineca.it\)/,/<\/file>/{x;d}; H; /<\/file>/{x;p}' metalink > metalinks/all.metalink
