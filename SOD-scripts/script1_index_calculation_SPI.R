9e5fcf65dc7c5b0e582731797520b56ee0ae8e9e
Rodrigo Manzanas <rmanzanas@users.noreply.github.com>
2020-09-28 13:30:25 +0200

    Update script1_index_calculation_SPI.R
    Minor corrections on comments

--- SOD-scripts/script1_index_calculation_SPI.R
+++ SOD-scripts/script1_index_calculation_SPI.R
@@ -1,3 +1,22 @@
+#     script1_index_calculation_SPI.R Generate SPI6-12 (see table below) NetCDF4 files from CMIP5 and CMIP6 Model Outputs for Atlas Product Reproducibility
+#
+#     Copyright (C) 2020 Santander Meteorology Group (http://www.meteo.unican.es)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 # This script builds on the climate4R framework 
 # https://github.com/SantanderMetGroup/climate4R
 
