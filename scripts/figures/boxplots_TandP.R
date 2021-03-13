66bd6930a9fd98f87df232d23200db879b08b533
Your Name <maibide@gmail.com>
2021-03-13 17:13:32 +0100

    minor

--- scripts/figures/boxplots_TandP.R
+++ scripts/figures/boxplots_TandP.R
@@ -39,23 +39,23 @@ ref.period <- 1995:2014
 # ref.period <- 1986:2005
 # ref.period <- 1850:1900
 # select the area, i.e. "land", "sea" or "landsea"
-area <- "landsea"
+area <- "land"
 # Select reference regions.  Select the CORDEX domain to be considered
-regions <- c("NWN","NEN","WNA","CNA","ENA", "NCA"); cordex.domain <- "NAM"
-regions <- c("NWS","NSA", "SAM", "NES", "SWS", "SES", "SSA"); cordex.domain <- "SAM"
-regions <- c("NCA", "SCA", "CAR"); cordex.domain <- "CAM"
-regions <- c("MED","SAH","WAF","CAF","NEAF", "SEAF", "WSAF", "ESAF", "MDG"); cordex.domain <- "AFR"
+regions <- c("NWN","NEN","GIC","RAR"); cordex.domain <- "ARC"
+regions <- c("SEA"); cordex.domain <- "SEA"
+# regions <- c("NWN","NEN","WNA","CNA","ENA", "NCA"); cordex.domain <- "NAM"
+# regions <- c("NWS","NSA", "SAM", "NES", "SWS", "SES", "SSA"); cordex.domain <- "SAM"
+# regions <- c("NCA", "SCA", "CAR"); cordex.domain <- "CAM"
+# regions <- c("MED","SAH","WAF","CAF","NEAF", "SEAF", "WSAF", "ESAF", "MDG"); cordex.domain <- "AFR"
 # regions <- c("ECA", "EAS"); cordex.domain <- "EAS"
-# regions <- c("SEA"); cordex.domain <- "SEA"
 # regions <- c("WCA","TIB", "ARP", "SAS"); cordex.domain <- "WAS"
 # regions <- c("EEU","WSB","ESB","RFE", "WCA", "ECA"); cordex.domain <- FALSE
 # regions <- c("SEA", "NAU","CAU","EAU","SAU","NZ"); cordex.domain <- "AUS"
 # regions <- c("WAN","EAN"); cordex.domain <- "ANT"
 # regions <- c("NEU","WCE","EEU","MED"); cordex.domain <- "EUR"
-# regions <- c("NWN","NEN","GIC","RAR"); cordex.domain <- "ARC"
 # regions <- c("ARO"); cordex.domain <- "ARC"
 # regions <- c("TIB", "SAS"); cordex.domain <- "WAS"
-regions <- c("world"); cordex.domain <- FALSE
+# regions <- c("world"); cordex.domain <- FALSE
 
 
 
@@ -64,8 +64,8 @@ ylim <- c(0, 6)
 xlim <- c(-50, 50)
 ylim <- NULL
 xlim <- NULL
-ylim <- c(-2, 8)
-xlim <- c(-5, 15)
+# ylim <- c(-2, 8)
+# xlim <- c(-5, 15)
 
 
 a <- computeFigures(regions = regions,
