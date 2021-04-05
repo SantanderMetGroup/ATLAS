48061cc5b313119e9983fd00b4a41df768b9d5a7
zequihg50 <ezequiel.cimadevilla@unican.es>
2021-04-05 10:53:17 +0200

    Generated EUR-11 interpolated ncmls

--- ESGF-inventory/CORDEX/EUR-11/classify.sh
+++ ESGF-inventory/CORDEX/EUR-11/classify.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-prefix=/gpfs/projects/meteo/DATA/CORDEX/synda/
+#prefix=/gpfs/projects/meteo/DATA/CORDEX/synda/
+prefix=/gpfs/external/IPCC/2018_IPCC/data/CORDEX/interp
 dest=datasets
 
-find_fxs() {
-    reference=${1#${prefix}}
-    reference=$(echo ${reference} | cut -d/ -f1-9)
-    reference=$(echo ${reference} | awk -F/ 'BEGIN{OFS="/"} {$7="r0i0p0"; print}')
-    reference=${prefix}${reference}
-    
-    if [ -d ${reference} ] ; then
-        find ${reference} -type f
-    fi
+mkdir -p ${dest}
+find "${prefix}/EUR-11" -type f | awk -F/ '
+BEGIN{
+  OFS="/"
 }
-
-find ${prefix} -mindepth 10 -maxdepth 10 | while read d
+{
+  $NF=""
+  $(NF-1)=""
+  $(NF-2)=""
+  sub(/\/+$/, "")
+  print
+}' | sort -u | grep -v r0i0p0 | while read d
 do
-    dataset=${d#${prefix}}
-    
-    # If it is a directory with fx just ignore
-    if $(echo ${dataset} | awk -F/ '$10=="fx"{exit(1)}') ; then
-        dataset=${dataset//\//_}
-        find ${d} -type f > ${dest}/${dataset}
-        find_fxs ${d} >> ${dest}/${dataset}
-    fi
+  dataset=${d#${prefix}/}
+  dataset=${dataset//\//_}
+  find ${d} -type f > ${dest}/${dataset}
+
+  fxs=$(echo ${d} | awk -F/ 'BEGIN{OFS="/"}{$NF="fx";$(NF-3)="r0i0p0";print}')
+  if [ -d ${fxs} ] ; then
+    find ${fxs} -type f >> ${dest}/${dataset}
+  fi
 done
