2a121530254723ad07dfc86185ffbdfe479cb0f3
Your Name <maibide@gmail.com>
2021-06-04 14:33:24 +0200

    change source branch to devel

--- aggregated-datasets/scripts/computeOffset.R
+++ aggregated-datasets/scripts/computeOffset.R
@@ -53,12 +53,12 @@ computeOffset <- function(project,
   
   # root Url
   # https://stackoverflow.com/questions/25485216/how-to-get-list-files-from-a-github-repository-folder-using-r
-  myurl <- "https://api.github.com/repos/SantanderMetGroup/ATLAS/git/trees/mai-devel?recursive=1"
+  myurl <- "https://api.github.com/repos/SantanderMetGroup/ATLAS/git/trees/devel?recursive=1"
   
   ## for remote
   req <- GET(myurl) %>% stop_for_status(req)
   filelist <- unlist(lapply(content(req)$tree, "[", "path"), use.names = FALSE)
-  root <- "https://raw.githubusercontent.com/SantanderMetGroup/ATLAS/mai-devel/"
+  root <- "https://raw.githubusercontent.com/SantanderMetGroup/ATLAS/devel/"
   
   ## local option-----------
   # root <- "/home/maialen/WORK/GIT/ATLAS/"
