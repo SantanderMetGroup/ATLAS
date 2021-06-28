dc2182d916606ec08ee06db70f90b24110f5d884
Maialen Iturbide <maibide@gmail.com>
2021-06-28 18:34:54 +0200

    minor

--- datasets-aggregated-regionally/scripts/computeDeltas.R
+++ datasets-aggregated-regionally/scripts/computeDeltas.R
@@ -16,9 +16,11 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 #' @description Compute temperature and precipitation changes from data 
 #' files of this repository (aggregated-datasets).
 #' @author M. Iturbide
+
 
 computeDeltas <- function(project, 
                           var,
