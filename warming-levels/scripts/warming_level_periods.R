fd700150fb6ba48270fc684414028a035aeb18c7
Jesus Fernandez <Jesus.Fernandez@unican.es>
2021-07-14 11:50:09 +0200

    Change license header

--- warming-levels/scripts/warming_level_periods.R
+++ warming-levels/scripts/warming_level_periods.R
@@ -1,19 +1,9 @@
 # warming_level_periods.R
 #
-# Copyright (C) 2019 Santander Meteorology Group (http://www.meteo.unican.es)
+# Copyright (C) 2021 Santander Meteorology Group (http://www.meteo.unican.es)
 #
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-# 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-# 
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This work is licensed under a Creative Commons Attribution 4.0 International
+# License (CC BY 4.0 - http://creativecommons.org/licenses/by/4.0)
 
 #' @title Global Warming Level timing to CSV
 #' @description Computation of central year for different GWLs using getGWL.R
