1f7920e84315c80a33f113357701fdd544846ec7
Maialen Iturbide <maibide@gmail.com>
2021-07-15 18:53:49 +0200

    add lines to generaty binary masks

--- reference-grids/scripts/referenceGridsCreation.R
+++ reference-grids/scripts/referenceGridsCreation.R
@@ -15,7 +15,9 @@
 library(loadeR)
 library(loadeR.2nc)
 library(transformeR)
+library(magrittr)
 
+# CREATE REFERENCE GRIDS -------------------------------------------------------
 # Data is accessed remotely from the Santander Climate Data Service
 # Obtain a user and password at http://meteo.unican.es/udg-wiki
 loginUDG(username = "yourUser", password = "yourPassword")
@@ -40,7 +42,21 @@ mask1 <- upscaleGrid(mask, times = 2, aggr.fun = list(FUN = mean))
 mask2 <- upscaleGrid(mask, times = 4, aggr.fun = list(FUN = mean))
 
 # export NetCDF 
-out.dir <- ""
+out.dir <- "../"
 grid2nc(mask, NetCDFOutFile = paste0(out.dir, "land_sea_mask_05degree.nc4"))
 grid2nc(mask1, NetCDFOutFile = paste0(out.dir, "land_sea_mask_1degree.nc4"))
 grid2nc(mask2, NetCDFOutFile = paste0(out.dir, "land_sea_mask_2degree.nc4"))
+
+
+
+# CREATE REFERENCE BINARY GRIDS -------------------------------------------------------
+
+ERA.mask <- loadGridData("../land_sea_mask_025degree_ERA5.nc", var = "lsm")
+land_sea_mask_025degree_binary <- binaryGrid(ERA.mask, condition = "GT", threshold = 0.5, values = c(0, 1)) %>% redim(drop = T)
+
+deg1.mask <- loadGridData("../land_sea_mask_1degree.nc4", var = "sftlf")
+land_sea_mask_1degree_binary <- binaryGrid(deg1.mask, condition = "GT", threshold = 0.5, values = c(0, 1)) %>% redim(drop = T)
+
+deg2.mask <- loadGridData("../land_sea_mask_2degree.nc4", var = "sftlf")
+land_sea_mask_2degree_binary <- binaryGrid(deg2.mask, condition = "GT", threshold = 0.5, values = c(0, 1)) %>% redim(drop = T)
+
